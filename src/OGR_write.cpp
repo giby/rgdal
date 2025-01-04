6d7613ec812b3b74c36c1abf0404fb41556b857c
Benjamin Leduc <giby@users.noreply.github.com>
2025-01-04 17:24:58 +0100

    Update OGR_write.cpp

--- src/OGR_write.cpp
+++ src/OGR_write.cpp
@@ -23,36 +23,36 @@ extern "C" {
 }
 
 SEXP writeOGR(SEXP dsn, SEXP layer, SEXP driver, SEXP ds_options, SEXP lco, SEXP lyr, SEXP overwrite, SEXP verbose, SEXP dataset_options, SEXP driver_options) {
-    OGRSFDriver *poDriver;
-    OGRDataSource *poDS;
+    GDALDriver *poDriver;
+    GDALDataset *poDS;
     OGRLayer *poLayer;
     OGRFeatureDefn *poDefn;
     char **papszCreateOptions = NULL;
     char **papszCreateOptionsLayer = NULL;
-    int n = Rf_length(ds_options);
+    int n = length(ds_options);
     int i;
 
     for (i = 0; i < n; i++) {
         papszCreateOptions = CSLAddString(papszCreateOptions, CHAR(STRING_ELT(ds_options, i)));
     }
 
-    poDriver = OGRSFDriverRegistrar::GetRegistrar()->GetDriverByName(CHAR(STRING_ELT(driver, 0)));
+    poDriver = GetGDALDriverManager()->GetDriverByName(CHAR(STRING_ELT(driver, 0)));
     if (poDriver == NULL) {
         Rf_error("Driver not found");
     }
 
-    poDS = poDriver->CreateDataSource(CHAR(STRING_ELT(dsn, 0)), papszCreateOptions);
+    poDS = poDriver->Create(CHAR(STRING_ELT(dsn, 0)), 0, 0, 0, GDT_Unknown, papszCreateOptions);
     if (poDS == NULL) {
         Rf_error("Data source creation failed");
     }
 
     OGRSpatialReference *poSRS = new OGRSpatialReference;
 
-    if (Rf_length(lyr) > 0) {
-        poSRS->importFromWkt((char **)CHAR(STRING_ELT(lyr, 0)));
+    if (length(lyr) > 0) {
+        poSRS->importFromWkt(CHAR(STRING_ELT(lyr, 0)));
     }
 
-    n = Rf_length(lco);
+    n = length(lco);
     for (i = 0; i < n; i++) {
         papszCreateOptionsLayer = CSLAddString(papszCreateOptionsLayer, CHAR(STRING_ELT(lco, i)));
     }
@@ -71,7 +71,7 @@ SEXP writeOGR(SEXP dsn, SEXP layer, SEXP driver, SEXP ds_options, SEXP lco, SEXP
 
     CSLDestroy(papszCreateOptions);
     CSLDestroy(papszCreateOptionsLayer);
-    OGRDataSource::DestroyDataSource(poDS);
+    GDALClose(poDS);
 
     delete poSRS;
 
