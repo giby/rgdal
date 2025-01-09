57a21c9b92fe85ecb018c78170507ca0e360368e
Benjamin Leduc <giby@users.noreply.github.com>
2025-01-09 18:22:07 +0100

    Update ogr_geom.cpp

--- src/ogr_geom.cpp
+++ src/ogr_geom.cpp
@@ -1,3 +1,4 @@
+
 /* Copyright (c) 2006 Roger Bivand
 * Function using C API and based on v.in.ogr from GRASS by Radim Blazek
 * to read OGR vector geometry features */
@@ -12,6 +13,15 @@
 #include <Rinternals.h>
 #include <Rdefines.h>
 #include "rgdal.h"
+
+// Undefine the length macro to avoid conflict with C++ headers
+#undef length
+
+#include <cpl_string.h>
+#include <vector>
+#include <locale>
+#include <format>
+#include <string>
 
 #ifdef __cplusplus
 extern "C" {
@@ -61,10 +71,10 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
     j=-1;
     installErrorHandler();
     for (i = 0; i < navailable_layers; i++) {
-	Ogr_layer =  OGR_DS_GetLayer( Ogr_ds, i );
-	Ogr_featuredefn = OGR_L_GetLayerDefn(Ogr_layer);
-	if (strcmp((char *)OGR_FD_GetName(Ogr_featuredefn), 
-	    CHAR(STRING_ELT(layer, 0))) == 0) j = i; 
+    Ogr_layer =  OGR_DS_GetLayer( Ogr_ds, i );
+    Ogr_featuredefn = OGR_L_GetLayerDefn(Ogr_layer);
+    if (strcmp((char *)OGR_FD_GetName(Ogr_featuredefn), 
+        CHAR(STRING_ELT(layer, 0))) == 0) j = i; 
     }
     uninstallErrorHandlerAndTriggerError();
 
@@ -86,7 +96,7 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
 //    SET_VECTOR_ELT(VECTOR_ELT(ans, 0), 0,
     installErrorHandler();
     SET_STRING_ELT(VECTOR_ELT(ans, 0), 0, 
-	COPY_TO_USER_STRING(OGR_DS_GetName(Ogr_ds)));
+    COPY_TO_USER_STRING(OGR_DS_GetName(Ogr_ds)));
     uninstallErrorHandlerAndTriggerError();
 
     installErrorHandler();
@@ -98,7 +108,7 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
 //    SET_VECTOR_ELT(VECTOR_ELT(ans, 1), 0, 
     installErrorHandler();
     SET_STRING_ELT(VECTOR_ELT(ans, 1), 0,
-	COPY_TO_USER_STRING((char *)OGR_FD_GetName(Ogr_featuredefn)));
+    COPY_TO_USER_STRING((char *)OGR_FD_GetName(Ogr_featuredefn)));
     uninstallErrorHandlerAndTriggerError();
     SET_VECTOR_ELT(ans, 2, NEW_INTEGER(1));
 
@@ -145,81 +155,81 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
     i=0;
     installErrorHandler();
     while( (Ogr_feature = OGR_L_GetNextFeature(Ogr_layer)) != NULL ) {
-	    /* Geometry */
-	Ogr_geometry = OGR_F_GetGeometryRef(Ogr_feature);
-	with_z = 0;
+        /* Geometry */
+    Ogr_geometry = OGR_F_GetGeometryRef(Ogr_feature);
+    with_z = 0;
 /* TODO fix NULL reading */
-	if ( Ogr_geometry == NULL ||
+    if ( Ogr_geometry == NULL ||
           wkbFlatten(OGR_G_GetGeometryType(Ogr_geometry)) == 0) {
 /*	    error("NULL geometry found"); */
-	    INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
- 	} else {
-	    INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 0;
-	    dim = OGR_G_GetCoordinateDimension(Ogr_geometry);
-	    if (dim > 2) 
-		with_z = 1;
+        INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
+     } else {
+        INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 0;
+        dim = OGR_G_GetCoordinateDimension(Ogr_geometry);
+        if (dim > 2) 
+        with_z = 1;
         eType = wkbFlatten(OGR_G_GetGeometryType(Ogr_geometry));
 
-	INTEGER_POINTER(VECTOR_ELT(ans, 3))[i] =  (int) eType;
-	INTEGER_POINTER(VECTOR_ELT(ans, 5))[i] =  with_z;
+    INTEGER_POINTER(VECTOR_ELT(ans, 3))[i] =  (int) eType;
+    INTEGER_POINTER(VECTOR_ELT(ans, 5))[i] =  with_z;
 
-	if (eType == wkbPoint) {
-	    SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(1));
-	    if (with_z == 0) 
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
-		NEW_LIST(2));
-	    else SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
-		NEW_LIST(3));
+    if (eType == wkbPoint) {
+        SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(1));
+        if (with_z == 0) 
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
+        NEW_LIST(2));
+        else SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
+        NEW_LIST(3));
 
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 0,
-		NEW_NUMERIC(1));
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 1,
-		NEW_NUMERIC(1));
-	    NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
-		4), i), 0), 0))[0] = OGR_G_GetX(Ogr_geometry, 0);
-	    NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
-		4), i), 0), 1))[0] = OGR_G_GetY(Ogr_geometry, 0);
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 0,
+        NEW_NUMERIC(1));
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 1,
+        NEW_NUMERIC(1));
+        NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
+        4), i), 0), 0))[0] = OGR_G_GetX(Ogr_geometry, 0);
+        NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
+        4), i), 0), 1))[0] = OGR_G_GetY(Ogr_geometry, 0);
 
-	    if (with_z > 0) {
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
-		    4), i), 0), 2, NEW_NUMERIC(1));
-	        NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		    ans, 4), i), 0), 2))[0] = OGR_G_GetZ(Ogr_geometry, 0);
-	    }
-	} else if (eType == wkbLineString) {
-	  np = OGR_G_GetPointCount(Ogr_geometry);
+        if (with_z > 0) {
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 
+            4), i), 0), 2, NEW_NUMERIC(1));
+            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+            ans, 4), i), 0), 2))[0] = OGR_G_GetZ(Ogr_geometry, 0);
+        }
+    } else if (eType == wkbLineString) {
+      np = OGR_G_GetPointCount(Ogr_geometry);
           if (np > 0) {
-	    SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(1));
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
-		NEW_LIST(2));
+        SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(1));
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0, 
+        NEW_LIST(2));
 
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 0,
-		NEW_NUMERIC(np));
-	    SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 1,
-		NEW_NUMERIC(np));
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 0,
+        NEW_NUMERIC(np));
+        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 0), 1,
+        NEW_NUMERIC(np));
             for( j = 0; j < np; j++ ) {
-	        NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		    ans, 4), i), 0), 0))[j] = OGR_G_GetX(Ogr_geometry, j);
-	        NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		    ans, 4), i), 0), 1))[j] = OGR_G_GetY(Ogr_geometry, j);
-	    }
+            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+            ans, 4), i), 0), 0))[j] = OGR_G_GetX(Ogr_geometry, j);
+            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+            ans, 4), i), 0), 1))[j] = OGR_G_GetY(Ogr_geometry, j);
+        }
           } else INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
-	} else if (eType == wkbPolygon) {
-	  nr = OGR_G_GetGeometryCount(Ogr_geometry);
+    } else if (eType == wkbPolygon) {
+      nr = OGR_G_GetGeometryCount(Ogr_geometry);
           if (nr > 0) {
-	    SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(nr));
+        SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(nr));
             jcnt = 0;
-	    for (j=0; j < nr; j++) {
-	      hRing = OGR_G_GetGeometryRef(Ogr_geometry, j);
-	      np = OGR_G_GetPointCount(hRing);
+        for (j=0; j < nr; j++) {
+          hRing = OGR_G_GetGeometryRef(Ogr_geometry, j);
+          np = OGR_G_GetPointCount(hRing);
               if (np > 0) {
                 jcnt++;
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), j, 
-		    NEW_LIST(2));
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
-		    j), 0, NEW_NUMERIC(np));
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
-		    j), 1, NEW_NUMERIC(np));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), j, 
+            NEW_LIST(2));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
+            j), 0, NEW_NUMERIC(np));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
+            j), 1, NEW_NUMERIC(np));
 // added hole reporting since #0 is defined exterior, rest interior
 //                if (j == 0) INTEGER_POINTER(Hole)[0] = 0;
 //                else INTEGER_POINTER(Hole)[0] = 1;
@@ -231,13 +241,13 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
 //                    i), j), install("hole"));
 // appear to need to retrieve Hole attribute 121016
                 for(k = 0; k < np; k++) {
-	            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		        ans, 4), i), j), 0))[k] = OGR_G_GetX(hRing, k);
-	            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		        ans, 4), i), j), 1))[k] = OGR_G_GetY(hRing, k);
-	        }
+                NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                ans, 4), i), j), 0))[k] = OGR_G_GetX(hRing, k);
+                NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                ans, 4), i), j), 1))[k] = OGR_G_GetY(hRing, k);
+            }
               } 
-	    }
+        }
             if (jcnt == 0) INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
 // added comment since #0 is defined exterior, rest interior
             if (jcnt > 0 && do_comments) {
@@ -247,61 +257,91 @@ SEXP R_OGR_CAPI_features(SEXP dsn, SEXP layer, SEXP comments)
                 INTEGER_POINTER(VECTOR_ELT(VECTOR_ELT(ans, 7), i))[j] = ROFFSET;
             }
           } else INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
-	} else if (eType == wkbMultiLineString) {
-	  nm = OGR_G_GetGeometryCount(Ogr_geometry);
+    } else if (eType == wkbMultiLineString) {
+      nm = OGR_G_GetGeometryCount(Ogr_geometry);
           if (nm > 0) {
-	    SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(nm));
+        SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(nm));
             jcnt = 0;
-	    for(j = 0; j < nm; j++) {
-	      hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
-	      np = OGR_G_GetPointCount(hRingM);
+        for(j = 0; j < nm; j++) {
+          hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
+          np = OGR_G_GetPointCount(hRingM);
               if (np > 0) {
                 jcnt++;
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), j, 
-		    NEW_LIST(2));
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
-		    j), 0, NEW_NUMERIC(np));
-	        SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
-		    j), 1, NEW_NUMERIC(np));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), j, 
+            NEW_LIST(2));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
+            j), 0, NEW_NUMERIC(np));
+            SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i), 
+            j), 1, NEW_NUMERIC(np));
                 for(k = 0; k < np; k++) {
-	            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		        ans, 4), i), j), 0))[k] = OGR_G_GetX(hRingM, k);
-	            NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
-		        ans, 4), i), j), 1))[k] = OGR_G_GetY(hRingM, k);
-	        }
+                NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                ans, 4), i), j), 0))[k] = OGR_G_GetX(hRingM, k);
+                NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                ans, 4), i), j), 1))[k] = OGR_G_GetY(hRingM, k);
+            }
               }
-	    }
+        }
             if (jcnt == 0) INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
           } else INTEGER_POINTER(VECTOR_ELT(ans, 6))[i] = 1;
-	} else if (eType == wkbMultiPolygon) {
-	  nm = OGR_G_GetGeometryCount(Ogr_geometry);
+    } else if (eType == wkbMultiPolygon) {
+      nm = OGR_G_GetGeometryCount(Ogr_geometry);
           if (nm > 0) {
-	    for(j = 0, mp_count = 0; j < nm; j++) {
-		hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
-	        nr = OGR_G_GetGeometryCount(hRingM);
-		mp_count += nr;
-	    }
-	    SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(mp_count));
+        for(j = 0, mp_count = 0; j < nm; j++) {
+        hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
+            nr = OGR_G_GetGeometryCount(hRingM);
+        mp_count += nr;
+        }
+        SET_VECTOR_ELT(VECTOR_ELT(ans, 4), i, NEW_LIST(mp_count));
 // added comment since #0 is defined exterior, rest interior
             if (do_comments)
               SET_VECTOR_ELT(VECTOR_ELT(ans, 7), i, NEW_INTEGER(mp_count));
-	    mp_count = 0;
+        mp_count = 0;
             jcnt = 0;
-	    for(j = 0; j < nm; j++) {
-		hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
-	        nr = OGR_G_GetGeometryCount(hRingM);
+        for(j = 0; j < nm; j++) {
+        hRingM = OGR_G_GetGeometryRef(Ogr_geometry, j);
+            nr = OGR_G_GetGeometryCount(hRingM);
                 if (nr > 0) {
-	          for (k=0; k < nr; k++) {
-	            hRing = OGR_G_GetGeometryRef(hRingM, k);
-	            np = OGR_G_GetPointCount(hRing);
+              for (k=0; k < nr; k++) {
+                hRing = OGR_G_GetGeometryRef(hRingM, k);
+                np = OGR_G_GetPointCount(hRing);
                     if (np > 0) {
                       jcnt++;
-	              SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i),
+                  SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), i),
                         mp_count, NEW_LIST(2));
-	              SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), 
-			i), mp_count), 0, NEW_NUMERIC(np));
-	              SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), 
-			i), mp_count), 1, NEW_NUMERIC(np));
+                  SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4), 
+            i), mp_count), 0, NEW_NUMERIC(np));
+                  SET_VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4),         
+            i), mp_count), 1, NEW_NUMERIC(np));
+                      for(km = 0; km < np; km++) {
+                      NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                VECTOR_ELT(ans, 4), i), mp_count), 0))[km] = 
+                OGR_G_GetX(hRing, km);
+                      NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
+                VECTOR_ELT(ans, 4), i), mp_count), 1))[km] = 
+                OGR_G_GetY(hRing, km);
+                  }
+// added hole reporting since #0 is defined exterior, rest interior
+//                      if (k == 0) INTEGER_POINTER(Hole)[0] = 0;
+//                      else INTEGER_POINTER(Hole)[0] = 1;
+//                      if (!do_comments) INTEGER_POINTER(Hole)[0] = NA_INTEGER;
+//                      setAttrib(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans, 4),
+//                        i), mp_count), install("hole"), Hole);
+//                      Hole = getAttrib(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(ans,
+//                        4), i), mp_count), install("hole"));
+// appear to need to retrieve Hole attribute 121016
+                      if (k == 0) mp_count_k0 = mp_count;
+// added comment since #0 is defined exterior, rest interior
+                      if (do_comments) {
+                        if (k == 0) 
+                          INTEGER_POINTER(VECTOR_ELT(VECTOR_ELT(ans,
+                            7), i))[mp_count] = 0;
+                        else INTEGER_POINTER(VECTOR_ELT(VECTOR_ELT(ans,
+                            7), i))[mp_count] = mp_count_k0 + ROFFSET;
+
+
+
+
+			    
                       for(km = 0; km < np; km++) {
 	                  NUMERIC_POINTER(VECTOR_ELT(VECTOR_ELT(VECTOR_ELT(
 			    VECTOR_ELT(ans, 4), i), mp_count), 0))[km] = 
